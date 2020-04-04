3d59a173bd0d7fe5c159f78e04230a508726394a
crash4star-tester <nikitok36@gmail.com>
2020-04-05 01:13:38 +0300

    feat: working on virtual-keyboard

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 // Наборы символов
 
-const engSymb = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'up', 'Shift', 'Ctrl', 'Win', 'Alt', 'space', 'Alt', 'left', 'down', 'right', 'Ctrl'];
+const engSymb = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', '    ', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'up', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'left', 'down', 'right', 'Ctrl'];
 
-const engSymbAlternate = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del', 'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '\'', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'up', 'Shift', 'Ctrl', 'Win', 'Alt', 'space', 'Alt', 'left', 'down', 'right', 'Ctrl'];
+const engSymbAlternate = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', '    ', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del', 'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '\'', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'up', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'left', 'down', 'right', 'Ctrl'];
 
-const rusSymb = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'up', 'Shift', 'Ctrl', 'Win', 'Alt', 'space', 'Alt', 'left', 'down', 'right', 'Ctrl'];
+const rusSymb = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', '    ', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'up', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'left', 'down', 'right', 'Ctrl'];
 
-const rusSymbAlternate = ['Ё', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del', 'Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'up', 'Shift', 'Ctrl', 'Win', 'Alt', 'space', 'Alt', 'left', 'down', 'right', 'Ctrl'];
+const rusSymbAlternate = ['Ё', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', '    ', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del', 'Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'up', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'left', 'down', 'right', 'Ctrl'];
 
 const dataKeys = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 
@@ -69,10 +69,29 @@ btn.forEach((item) => {
 });
 
 
-// Работа клавиатуры
-document.addEventListener('keydown', function() {
+// Отображение нужных символов в area
+const area = document.querySelector('.keyboard__area');
+let userValue;
+let data = [];
+let text = '';
+
+
+area.oninput = function() {
+  if (localStorage.getItem('lang') === 'eng') {;
+    this.value.split('').forEach((symb) => {
+      data.push(engSymb[rusSymb.indexOf(symb)]);
+    });  
+    this.value = '';
+    text = data.join('');
+    this.value = text;
+  }
+  console.log(text)
+};
+
+
+// Работа кнопок
+document.addEventListener('keydown', function () {
   const key = document.querySelectorAll('.key');
-  const area = document.querySelector('.keyboard__area');
   area.focus();
 
   key.forEach((btn) => {
@@ -88,31 +107,6 @@ document.addEventListener('keydown', function() {
       };
     }
   });
-});
-
-// Проверка состояния caps lock
-document.addEventListener('keyup', () => {
-  const key = document.querySelectorAll('.key');
-
-  if (event.getModifierState('CapsLock')) {
-    let i = 0;
-    while (i < key.length) {
-      const data = key[i].getAttribute('data-key');
-      if (data == 'CapsLock') {
-        key[i].classList.add('key--press');
-      }
-      i++;
-    }
-  } else {
-    let i = 0;
-    while (i < key.length) {
-      const data = key[i].getAttribute('data-key');
-      if (data == 'CapsLock') {
-        key[i].classList.remove('key--press');
-      }
-      i++;
-    }
-  }
 });
 
 
@@ -137,13 +131,118 @@ document.addEventListener('keydown', (e) => {
 });
 
 
-// Проверка при загрузке страницы
-if (localStorage.getItem('lang') === 'eng') {
-  langBtns.forEach((lang, i) => {
-    lang.textContent = engSymb[i];
-  });
-} else {
-  langBtns.forEach((lang, i) => {
-    lang.textContent = rusSymb[i];
-  });
+// Caps включен
+function getUpperCase() {
+  if (localStorage.getItem('lang') === 'eng') {
+    langBtns.forEach((lang, i) => {
+      const data = lang.getAttribute('data-key');
+
+      if (data === 'Backspace' || data === 'Tab' || data === 'Delete' || data === 'CapsLock' || data === 'Enter' || data === 'ShiftLeft' || data === 'ShiftRight' || data === 'ControlLeft' || data === 'ControlRight' || data === 'AltLeft' || data === 'AltRight' || data === 'MetaLeft') {
+        lang.textContent = engSymb[i];
+      } else {
+        lang.textContent = engSymb[i].toUpperCase();
+      }
+    });
+  } else {
+    langBtns.forEach((lang, i) => {
+      const data = lang.getAttribute('data-key');
+
+      if (data === 'Backspace' || data === 'Tab' || data === 'Delete' || data === 'CapsLock' || data === 'Enter' || data === 'ShiftLeft' || data === 'ShiftRight' || data === 'ControlLeft' || data === 'ControlRight' || data === 'AltLeft' || data === 'AltRight' || data === 'MetaLeft') {
+        lang.textContent = rusSymb[i];
+      } else {
+        lang.textContent = rusSymb[i].toUpperCase();
+      }
+    });
+  }
 }
+
+
+// Caps выключен
+function getLowerCase() {
+  if (localStorage.getItem('lang') === 'eng') {
+    langBtns.forEach((lang, i) => {
+      const data = lang.getAttribute('data-key');
+
+      if (data === 'Backspace' || data === 'Tab' || data === 'Delete' || data === 'CapsLock' || data === 'Enter' || data === 'ShiftLeft' || data === 'ShiftRight' || data === 'ControlLeft' || data === 'ControlRight' || data === 'AltLeft' || data === 'AltRight' || data === 'MetaLeft') {
+        lang.textContent = engSymb[i];
+      } else {
+        lang.textContent = engSymb[i].toLowerCase();
+      }
+    });
+  } else {
+    langBtns.forEach((lang, i) => {
+      const data = lang.getAttribute('data-key');
+
+      if (data === 'Backspace' || data === 'Tab' || data === 'Delete' || data === 'CapsLock' || data === 'Enter' || data === 'ShiftLeft' || data === 'ShiftRight' || data === 'ControlLeft' || data === 'ControlRight' || data === 'AltLeft' || data === 'AltRight' || data === 'MetaLeft') {
+        lang.textContent = rusSymb[i];
+      } else {
+        lang.textContent = rusSymb[i].toLowerCase();
+      }
+    });
+  }
+}
+
+
+document.addEventListener('keydown', (e) => {
+  const key = document.querySelectorAll('.key');
+
+  // Проверка состояния caps lock
+  if (e.getModifierState('CapsLock')) {
+    key.forEach((btn) => {
+      if (btn.getAttribute('data-key') === 'CapsLock') {
+        btn.style.background = 'green';
+      }
+    });
+    getUpperCase();
+  } else {
+    key.forEach((btn) => {
+      if (btn.getAttribute('data-key') === 'CapsLock') {
+        btn.style.background = '';
+      }
+    });
+    getLowerCase();
+  }
+
+  // Отмена alt
+  if (e.key === 'Alt') {
+    e.preventDefault();
+  }
+});
+
+
+// Клик по tab
+HTMLTextAreaElement.prototype.getCaret = function () {
+  return this.selectionStart;
+};
+
+HTMLTextAreaElement.prototype.setCaret = function (position) {
+  this.selectionStart = position;
+  this.selectionEnd = position;
+  this.focus();
+};
+
+
+window.onkeydown = function (e) {
+  if (e.key === 'Tab') {
+    let newCaretPosition = 0;
+
+    newCaretPosition = area.getCaret() + '    '.length;
+    area.value = `${area.value.substring(0, area.getCaret())}    ${area.value.substring(area.getCaret(), area.value.length)}`;
+    area.setCaret(newCaretPosition);
+    return false;
+  }
+};
+
+
+window.addEventListener('load', (e) => {
+  // Проверка языка при загрузке страницы
+  if (localStorage.getItem('lang') === 'eng') {
+    langBtns.forEach((lang, i) => {
+      lang.textContent = engSymb[i];
+    });
+  } else {
+    langBtns.forEach((lang, i) => {
+      lang.textContent = rusSymb[i];
+    });
+  }
+});
